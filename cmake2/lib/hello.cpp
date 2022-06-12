3c3a0d8a92598de1da00a54956e25f82c614a3d0
Apuuu <466007639@qq.com>
2022-06-13 02:18:06 +0800

    first commit

---
+++ cmake2/lib/hello.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Apuuu on 2022/6/13.
+//
+
+#include "hello.h"
+#include <iostream>
+void HelloFunc(){
+    std::cout << "Hello World cmake" << std::endl;
+}
---
+++ cmake2/lib/hello.h
@@ -0,0 +1,10 @@
+//
+// Created by Apuuu on 2022/6/13.
+//
+
+#ifndef HELLO_HELLO_H
+#define HELLO_HELLO_H
+
+void HelloFunc();
+
+#endif //HELLO_HELLO_H
